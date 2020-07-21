e1d757a644899d11f596e5c02d9d98d9cb1a15cd
Vismith Adappa <vismith.24.adappa@gmail.com>
2020-07-21 17:28:36 +0530

    Added username in profile schema

--- Schemas/profile.js
+++ Schemas/profile.js
@@ -3,8 +3,13 @@ const mongoose = require('mongoose');
 var profileSchema = new mongoose.Schema({
     picture: {
         type: String,
+        default: 'https://cdn1.iconfinder.com/data/icons/user-pictures/100/unknown-512.png'
     },
     email: {
+        type: String,
+        unique: true
+    },
+    username: {
         type: String,
         unique: true
     }
