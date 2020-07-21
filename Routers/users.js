64e81ccad08f4cf4212959448b36aa0240b35423
Vismith Adappa <vismith.24.adappa@gmail.com>
2020-07-21 17:28:03 +0530

    Fixed /users route

--- Routers/users.js
+++ Routers/users.js
@@ -2,15 +2,32 @@ var express = require('express');
 var router = express.Router();
 var userController = require('../Controllers/userController');
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
+var profileModel = require('../Schemas/profile');
 
 router.get('/username', userController.get_username);
 router.get('/email', userController.get_email);
-router.get("/", passport.authenticate("jwt", { session: false }), (req, res) => {
-    res.json({
-      username: req.user.username,
-      email: req.user.email,
-      picture: req.user.picture,
-    });
+router.get("/", async (req, res) => {
+  if (req.headers.authorization) {
+    var token = req.headers.authorization.split(' ')[1];
+    var payload = jwt.decode(token, "nodeauthsecret");
+    var email = payload.email;
+    console.log(payload);
+    await profileModel.findOne({email: email}, (err, result) => {
+      if (err) {
+        res.status(501).end();
+        throw err;
+      }
+      res.json({
+        username: result.username,
+        email: result.email,
+        picture: result.picture
+      })
+    })
+  }
+  else {
+    res.json({});
+  }
 });
 
 module.exports = router;
