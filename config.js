f95a7689d98aa509228406298df503eec0d2fa78
Vismith Adappa <vismith.24.adappa@gmail.com>
2020-07-22 08:24:48 +0530

    Added secrets

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 module.exports = {
     RAZOR_PAY_KEY_ID: "rzp_test_1lsAAogLRJ2KFu",
-    RAZOR_PAY_KEY_SECRET: "IpFaXfpcM7omkJKBlJEhFdNZ"
+    RAZOR_PAY_KEY_SECRET: "IpFaXfpcM7omkJKBlJEhFdNZ",
+    CUATRO_MAIL_ID: "cuatrotechinc@gmail.com",
+    CUATRO_MAIL_PASSWORD: "cuatroinc123",
+    MONGO_DB_CONN_STRING: "mongodb+srv://cuatro:cuatro-admin@cuatro.k6i2p.mongodb.net/CUATRO?retryWrites=true&w=majority"
 }
