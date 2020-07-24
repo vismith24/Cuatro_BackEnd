925b2a7c685ee6fa9627f1ee85683cfb3cabf121
Vismith Adappa <vismith.24.adappa@gmail.com>
2020-07-25 02:27:56 +0530

    Playlist API corrected

--- Controllers/playlistController.js
+++ Controllers/playlistController.js
@@ -5,12 +5,11 @@ const jwt = require('jsonwebtoken')
 
 exports.createPlaylist = async (req, res) => {
     if (req.headers.authorization) {
-        console.log('auth', req.headers.authorization)
         var token = req.headers.authorization.split(' ')[1];
         var payload = jwt.decode(token, "nodeauthsecret");
         var email = payload.email;
         var user, newPlayist;
-        await profileModel.findOne({email: email}, (err, result) => {
+        await profileModel.findOne({email: email}, async (err, result) => {
             if (err) {
                 res.status(500).end();
                 throw err;
@@ -18,16 +17,16 @@ exports.createPlaylist = async (req, res) => {
             user = result;
             var { name } = req.body
             newPlayist = new playlistModel({name: name, profile: user._id});
-        });
-        console.log(user)
-        console.log(req.body)
-        await newPlayist.save((err) => {
-            if (err) {
-                res.status(500).end()
-                throw err;
-            }
-        })
+            console.log(user)
+            console.log(req.body)
+            await newPlayist.save((err) => {
+                if (err) {
+                    res.status(500).end()
+                    throw err;
+                }
+            })
         res.send("Playlist added")
+        });
     }
     else {
         res.json({})
@@ -40,21 +39,22 @@ exports.getAll = async (req, res) => {
         var token = req.headers.authorization.split(' ')[1];
         var payload = jwt.decode(token, "nodeauthsecret");
         var email = payload.email;
+        console.log(email);
         var user;
-        await profileModel.findOne({email: email}, (err, result) => {
+        await profileModel.findOne({email: email}, async (err, result) => {
             if (err) {
                 res.status(500).end();
                 throw err;
             }
             user = result;
-        });
-        console.log(req.body)
-        await playlistModel.find({}, 'name', (err, results) => {
-            if (err) {
-                res.status(500).end();
-                throw err;
-            }
-            res.send(results)
+            await playlistModel.find({profile: user._id}).populate('songs').exec( (err, results) => {
+                if (err) {
+                    res.status(500).end();
+                    throw err;
+                }
+                console.log(results);
+                res.send(results)
+            })
         })
         // res.send("Playlist added")
     }
@@ -69,50 +69,49 @@ exports.addSongToPlaylist = async (req, res) => {
         var payload = jwt.decode(token, "nodeauthsecret");
         var email = payload.email;
         var user;
-        await profileModel.findOne({email: email}, (err, result) => {
+        await profileModel.findOne({email: email}, async (err, result) => {
             if (err) {
                 res.status(500).end();
                 throw err;
             }
             user = result;
-        });
-        console.log(req.body, user)
-        const { name, music } = req.body
-        var songs;
-        await musicModel.findById(music._id, (err, results) =>{
-            if (err) {
-                res.status(500).end();
-                throw err;
-            }
-            if (res == null) {
-                res.status(400).end()
-            }
-        })
-        await playlistModel.findOne({ name: name, profile: user._id }, (err, results) => {
-            if (err) {
-                res.status(500).end();
-                throw err;
-            }
-            console.log('jfklasdf', results)
-            songs = results.songs
-            if (songs){
-                if (songs.includes(music._id))
-                    res.send("Already in playlist")
-                else
-                    songs.push(music._id)
-            }
-            else {
-                songs = [music._id]
-            }
-        })
-        
-        await playlistModel.findOneAndUpdate({ name: name, profile: user}, { songs: songs }, (err, results) =>{
-            if (err) {
-                res.status(500).end();
-                throw err;
-            }
-            // console.log(res)
-            res.send("Added to playlist")
+                //console.log(req.body, user)
+                const { name, music } = req.body
+                var songs;
+                await musicModel.findById(music._id, (err, results) =>{
+                    if (err) {
+                        res.status(500).end();
+                        throw err;
+                    }
+                    if (res == null) {
+                        res.status(400).end()
+                    }
+                })
+                await playlistModel.findOne({ name: name, profile: user._id }, async (err, results) => {
+                    if (err) {
+                        res.status(500).end();
+                        throw err;
+                    }
+                    //console.log('jfklasdf', results)
+                    songs = results.songs
+                    if (songs){
+                        if (songs.includes(music._id))
+                            res.send("Already in playlist")
+                        else
+                            songs.push(music._id)
+                    }
+                    else {
+                        songs = [music._id]
+                    }
+                    await playlistModel.findOneAndUpdate({ name: name, profile: user}, { songs: songs }, (err, results) =>{
+                        if (err) {
+                            res.status(500).end();
+                            throw err;
+                        }
+                        // console.log(res)
+                        res.send("Added to playlist")
+                    })
+                })
         })
         // res.send("Playlist added")
     }
